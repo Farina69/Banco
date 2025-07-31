992871ddbab97a5419fffc706363f0bf2e4fe343
ra2457013 <ra2457013@LDI315>
2025-07-30 21:28:56 -0300

    Add project files.

---
+++ Banco/Conta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    public class Conta
+    {
+        //def dos atributos intesno da classe
+        private Int32 Numero;
+        private decimal LimiteCredito;
+        private decimal Saldo;
+        private int contadorConta;
+
+
+        //criacao das prop da classe
+        public Int32 numero {  
+            get { return Numero; }
+            set {  Numero = value; }
+            }
+        public decimal limiteCredito
+        {
+            get { return LimiteCredito;  }
+            set { LimiteCredito = value; }
+        }
+
+        //criacao dos metodos classe conta
+        public decimal saldo
+        {
+            get { return Saldo; }
+            set { Saldo = value; }
+        }
+        public void Sacar(decimal valor)
+        {
+            //contadorConta++;
+            saldo -= valor;
+
+        }
+        public void Depositar(decimal valor)
+        {
+            //contadorConta++;
+            saldo += valor;
+        }
+        //metodo construtor da classe conta
+    }
+}
---
+++ Banco/frmCaixaEletronico.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Banco
+{
+    partial class frmCaixaEletronico
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numoperSaque = new System.Windows.Forms.NumericUpDown();
+            this.btnSaque = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numoperSaque)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Saldo:";
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Location = new System.Drawing.Point(94, 22);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(40, 20);
+            this.lblSaldo.TabIndex = 1;
+            this.lblSaldo.Text = "0,00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(34, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor Saque:";
+            // 
+            // numoperSaque
+            // 
+            this.numoperSaque.Location = new System.Drawing.Point(141, 74);
+            this.numoperSaque.Maximum = new decimal(new int[] {
+            1874919423,
+            2328306,
+            0,
+            0});
+            this.numoperSaque.Name = "numoperSaque";
+            this.numoperSaque.Size = new System.Drawing.Size(120, 20);
+            this.numoperSaque.TabIndex = 3;
+            // 
+            // btnSaque
+            // 
+            this.btnSaque.Location = new System.Drawing.Point(38, 114);
+            this.btnSaque.Name = "btnSaque";
+            this.btnSaque.Size = new System.Drawing.Size(223, 23);
+            this.btnSaque.TabIndex = 4;
+            this.btnSaque.Text = "Sacar";
+            this.btnSaque.UseVisualStyleBackColor = true;
+            this.btnSaque.Click += new System.EventHandler(this.btnSaque_Click);
+            // 
+            // frmCaixaEletronico
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaque);
+            this.Controls.Add(this.numoperSaque);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSaldo);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCaixaEletronico";
+            this.Text = "Caixa Eletrônico";
+            this.Load += new System.EventHandler(this.frmCaixaEletronico_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numoperSaque)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSaldo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numoperSaque;
+        private System.Windows.Forms.Button btnSaque;
+    }
+}
+
